28039b7d93f7f3dc7b1621c362a2560506fcd937
Arjun Goel <arjungoel1995@gmail.com>
2024-02-06 10:49:53 +0000

    added new table in db

--- dbo/Tablea.sql
+++ dbo/Tablea.sql
@@ -2,18 +2,10 @@ CREATE TABLE [dbo].[Tablea] (
     [col1] INT NOT NULL,
     [colb] VARCHAR (10) NULL,
     [colc] VARCHAR (10) NULL
-);
+)
 
 
 
 GO
-
-INSERT INTO Tablea (col1, colb, colc) values (1, '', 'abc');
-
-GO
-
-INSERT INTO Tablea (col1, colb, colc) values (2, '', 'bcd');
-
-
 
 GO
