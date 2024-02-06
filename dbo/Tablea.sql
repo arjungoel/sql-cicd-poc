a6b07067eb6d561d8863ac11ff06fad873ae9c57
Arjun Goel <arjungoel1995@gmail.com>
2024-02-06 10:41:09 +0000

    updated sql code

--- dbo/Tablea.sql
+++ dbo/Tablea.sql
@@ -2,14 +2,14 @@ CREATE TABLE [dbo].[Tablea] (
     [col1] INT NOT NULL,
     [colb] VARCHAR (10) NULL,
     [colc] VARCHAR (10) NULL
-)
+);
 
 
 
 GO
 
 INSERT INTO Tablea (col1, colb, colc) values (1, '', 'abc');
-INSERT INTO Tablea (col1, colb, colc) values (2, '', 'bcd')
+INSERT INTO Tablea (col1, colb, colc) values (2, '', 'bcd');
 
 
 
