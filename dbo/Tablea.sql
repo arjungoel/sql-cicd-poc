94ac1945a37cab3c74ae10d6b4094161cea52dc9
Arjun Goel <arjungoel1995@gmail.com>
2024-02-06 10:32:09 +0000

    added INSERT commands

--- dbo/Tablea.sql
+++ dbo/Tablea.sql
@@ -1,11 +1,15 @@
 CREATE TABLE [dbo].[Tablea] (
     [col1] INT NOT NULL,
-    [colb] VARCHAR (10) NULL
+    [colb] VARCHAR (10) NULL,
+    [colc] VARCHAR (10) NULL
 )
 
 
 
 GO
+
+INSERT INTO Tablea (col1, colb, colc) values (1, '', 'abc');
+INSERT INTO Tablea (col1, colb, colc) values (2, '', 'bcd')
 
 
 
