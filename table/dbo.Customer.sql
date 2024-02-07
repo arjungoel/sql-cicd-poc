4a665f0337f3b730f5e3d5e934c3f6c4360e1e2e
Arjun Goel <arjungoel1995@gmail.com>
2024-02-07 05:54:13 +0000

    fix the syntax for insert command

--- table/dbo.Customer.sql
+++ table/dbo.Customer.sql
@@ -1,13 +1,13 @@
 CREATE TABLE [dbo].[Customer]
 (
-[Id] [int] NOT NULL IDENTITY(1, 1),
-[Name] [nvarchar] (50) NOT NULL,
-[Address] [nvarchar] (200) NOT NULL,
-[CreateDate] [datetime] NOT NULL
+    [Id] [int] NOT NULL IDENTITY(1, 1),
+    [Name] [nvarchar](50) NOT NULL,
+    [Address] [nvarchar](200) NOT NULL,
+    [CreateDate] [datetime] NOT NULL
 ) ON [PRIMARY]
 
 GO
 
-INSERT INTO TABLE [dbo].[Customer] values (1, 'Arjun', 'Sonipat', '2024-02-07')
+INSERT INTO [dbo].[Customer] ([Name], [Address], [CreateDate]) VALUES ('Arjun', 'Sonipat', '2024-02-07')
 
 GO
