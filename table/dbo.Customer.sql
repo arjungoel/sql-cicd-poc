77f343ced26306bc23de705d3a02602cb459354d
Arjun Goel <arjungoel1995@gmail.com>
2024-02-07 09:34:08 +0000

    Added SQL INSERT command

--- table/dbo.Customer.sql
+++ table/dbo.Customer.sql
@@ -10,3 +10,7 @@ GO
 INSERT INTO [Customer] ([Name], [Address], [CreateDate]) VALUES ('Arjun', 'Sonipat', '2024-02-07 00:00:00')
 
 GO
+
+INSERT INTO [Customer] ([Name], [Address], [CreateDate]) VALUES ('Harsh', 'Chennai', '2024-02-07 00:00:00')
+
+GO
