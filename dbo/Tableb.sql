099dc1321f1f19a90b8f1e58bc39c8d153df5c9b
Arjun Goel <arjungoel1995@gmail.com>
2024-02-07 05:32:24 +0000

    Drop db table

--- dbo/Tableb.sql
+++ dbo/Tableb.sql
@@ -1,11 +1,16 @@
-CREATE TABLE [dbo].[Tableb] (
-    [col1] INT NOT NULL,
-    [colb] VARCHAR (10) NULL,
-    [colc] VARCHAR (10) NULL,
-    [cold] VARCHAR (10) NULL
-)
+-- CREATE TABLE [dbo].[Tableb] (
+--     [col1] INT NOT NULL,
+--     [colb] VARCHAR (10) NULL,
+--     [colc] VARCHAR (10) NULL,
+--     [cold] VARCHAR (10) NULL
+-- )
 
 
+
+-- GO
+
+-- GO
+DROP TABLE [dbo].[Tableb]
 
 GO
 
