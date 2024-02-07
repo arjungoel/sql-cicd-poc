0e23f11693654d52741ff7ed7ec0b744ca730d28
Arjun Goel <arjungoel1995@gmail.com>
2024-02-07 06:52:42 +0000

    Updated create table command

--- table/dbo.Customer.sql
+++ table/dbo.Customer.sql
@@ -1,5 +1,6 @@
 CREATE TABLE [dbo].[Customer]
 (
+    [Id] [int] NOT NULL IDENTITY(1, 1),
     [Name] [nvarchar](50) NOT NULL,
     [Address] [nvarchar](200) NOT NULL,
     [CreateDate] [datetime] NOT NULL
