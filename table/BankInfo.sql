3c28e08dbf37dc81dd7d78576198670d2a7a97c7
Arjun Goel <arjungoel1995@gmail.com>
2024-02-09 03:44:08 +0000

    Updated sql code and workflow

--- table/BankInfo.sql
+++ table/BankInfo.sql
@@ -1,12 +1,17 @@
-CREATE TABLE [dbo].[BankInfo]
-(
-    [BankName] [nvarchar](50) NOT NULL,
-    [BankAddress] [nvarchar](200) NOT NULL,
-    [BankIFSCCode] [nvarchar] (100) NOT NULL
-)
+IF NOT EXISTS (SELECT 1 FROM sys.tables WHERE name = 'BankInfo')
+BEGIN
+    CREATE TABLE [dbo].[BankInfo]
+    (
+        [BankName] [nvarchar](50) NOT NULL,
+        [BankAddress] [nvarchar](200) NOT NULL,
+        [BankIFSCCode] [nvarchar](100) NOT NULL
+    )
+END
 
-GO
-
-Insert into BankInfo Values ('SBI', 'Naraina', 'IFSC1234'), ('CitiBank', 'Naraina', 'IFSC5678'), ('HDFC', 'Sonipat', 'IFSC9012');
-
-GO
+INSERT INTO [dbo].[BankInfo] VALUES ('SBI6', 'Naraina6', 'IFSC512346');
+INSERT INTO [dbo].[BankInfo] VALUES ('SBI7', 'Naraina7', 'IFSC512347');
+INSERT INTO [dbo].[BankInfo] VALUES ('SBI8', 'Naraina8', 'IFSC512348');
+INSERT INTO [dbo].[BankInfo] VALUES ('SBI9', 'Naraina9', 'IFSC512349');
+INSERT INTO [dbo].[BankInfo] VALUES ('SBI0', 'Naraina10', 'IFSC512340');
+INSERT INTO [dbo].[BankInfo] VALUES ('SBI11', 'Naraina11', 'IFSC512341');
+INSERT INTO [dbo].[BankInfo] VALUES ('SBI12', 'Naraina11', 'IFSC512341');
